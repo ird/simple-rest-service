0cd1cfb506ddacfbb78c53a83c698ad46c333b7c
ird <31051194+ird@users.noreply.github.com>
2017-12-02 16:09:32 +0000

    FilmRequestController.java: Added @RestController mapped to /film

--- src/main/java/uk/org/ird/FilmRequestController.java
+++ src/main/java/uk/org/ird/FilmRequestController.java
@@ -1,4 +1,17 @@
 package uk.org.ird;
 
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
 public class FilmRequestController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/film")
+    public Film film(@RequestParam(value="name", defaultValue="Pulp Fiction") String name,
+                     @RequestParam(value="year", defaultValue="1994") int year) {
+        return new Film(counter.incrementAndGet(), name, year);
+    }
 }
