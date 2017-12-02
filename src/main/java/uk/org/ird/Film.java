8a0ab906bb5f025d5e2313bd4e866de929a225ce
ird <31051194+ird@users.noreply.github.com>
2017-12-02 13:18:41 +0000

    Film.java: POJO, Service.java: @SpringApplication

--- src/main/java/uk/org/ird/Film.java
+++ src/main/java/uk/org/ird/Film.java
@@ -1,4 +1,17 @@
 package uk.org.ird;
 
 public class Film {
+    private final long id;
+    private final String name;
+    private final int year;
+    public Film(long id, String name, int year) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+    }
+    public long getId() { return id; }
+    public String getFullTitle() {
+        String title = name + "(%s)";
+        return String.format(title, year);
+    }
 }
